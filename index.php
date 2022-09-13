7e13ef4dfe9d97b03efee7c5c06535d99820a001
kenethnic <79937993+kennethnico@users.noreply.github.com>
2022-09-13 13:25:22 -0500

    Actualización título

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <title>Asistencia | Foro Mujeres y Justicia </title>
+    <title>Asistencia | Feminicidio: Retos y desafíos en la impartición de justicia </title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--===============================================================================================-->
